cc2294488302279d6e15d964fdbe5038e79c7085
Luis Santiago Duque Franco <lduquef@unal.edu.co>
2025-06-12 23:22:58 -0500

    cubeconfig
    primera configuración librerías cubeIDE

---
+++ 08Tarea2/Src/main.c
@@ -0,0 +1,35 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : luisduquefranco
+ * @brief          : Tarea2
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include "gpio_driver_hal.h"
+#include "stm32_assert.h"
+#include "exti_driver_hal.h"
+#include "timer_driver_hal.h"
+#include "SysTickDriver.h"
+#include "stm32f4xx.h"
+
+
+
+
+int main(void)
+{
+    /* Loop forever */
+	for(;;);
+}
